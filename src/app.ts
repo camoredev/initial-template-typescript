335885dbaccfe9eb00a4c1dc5794cf2df92009ec
Caio Moraes <camoredev@gmail.com>
2022-12-14 19:37:46 -0300

    Adicona conexão com banco e scripts de dev e start

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+import "express-async-errors";
+import httpErrorMiddleware from "./middlewares/http.error.middleware";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use(httpErrorMiddleware);
+export default app;
--- src/model/db/connection.ts
+++ src/model/db/connection.ts
@@ -8,4 +8,5 @@ mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
 });
